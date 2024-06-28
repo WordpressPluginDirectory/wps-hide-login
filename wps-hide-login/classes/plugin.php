42dcdf28b6e784b833c2b4d502c66b9b5b572f30
Justin Gardner <justin@rhynorater.com>
2024-06-28 06:02:13 -0500

    1.9.16.7

--- wps-hide-login/classes/plugin.php
+++ wps-hide-login/classes/plugin.php
@@ -693,7 +693,7 @@ class Plugin {
 		}
 
 		if ( ! is_user_logged_in() ) {
-			if ( (  is_plugin_active( 'gravityforms/gravityforms.php' ) || ( is_multisite() && is_plugin_active_for_network( 'gravityforms/gravityforms.php' ) ) ) && isset( $_GET['gf_page'] ) ) {
+			if ( file_exists( WP_CONTENT_DIR . '/plugins/gravityforms/gravityforms.php' ) && isset( $_GET['gf_page'] ) ) {
 				return $origin_url;
 			}
 		}
